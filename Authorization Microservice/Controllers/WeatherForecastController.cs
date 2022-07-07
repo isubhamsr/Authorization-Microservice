f4ea30b04ad531a21205e760525b7008f3453766
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-07-07 05:46:40 +0530

    Implement authorization token will create on login

--- Authorization Microservice/Controllers/WeatherForecastController.cs
+++ Authorization Microservice/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Authorization_Microservice.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
